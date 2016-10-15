a75c517288cd94cc2bc3bbcba81078cb72e5fbd5
Javier Zalasar (Lenovo) <javier25arg@gmail.com>
2016-10-15 10:41:03 -0300

    Android App DOS
    Una nueva versión de la App de contactos.

--- app/src/main/java/com/example/javier/abmcontactos/Actividad1.java
+++ app/src/main/java/com/example/javier/abmcontactos/Actividad1.java
@@ -8,13 +8,17 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Actividad1 extends AppCompatActivity {
 
     //Variables a pasar
     private EditText nombre;
     private EditText telefono;
     private EditText eMail;
-    private EditText desc;
+    private EditText descCampo;
+    private int year, month, day;
     //Botón
     private Button boton;
     private DatePicker fecha;
@@ -28,7 +32,7 @@ public class Actividad1 extends AppCompatActivity {
         nombre=(EditText)findViewById(R.id.campoNombre);
         telefono=(EditText)findViewById(R.id.campoTelefono);
         eMail=(EditText)findViewById(R.id.campoMail);
-        desc=(EditText)findViewById(R.id.campoDesc);
+        descCampo=(EditText)findViewById(R.id.campoDesc);
         fecha=(DatePicker)findViewById(R.id.campoFecha);
 
         //Buscamos el boton
@@ -44,11 +48,19 @@ public class Actividad1 extends AppCompatActivity {
 
         Intent i=new Intent(this,Actividad2.class);
 
+            year = ((DatePicker) findViewById(R.id.campoFecha)).getYear();
+            month = ((DatePicker) findViewById(R.id.campoFecha)).getMonth();
+            day = ((DatePicker) findViewById(R.id.campoFecha)).getDayOfMonth();
+
             i.putExtra("nombre",nombre.getText().toString());
             i.putExtra("telefono",telefono.getText().toString());
-            i.putExtra("eMail",eMail.getText().toString());
-            i.putExtra("desc",desc.getText()).toString();
+            i.putExtra("eMail",eMail.getText().toString()).toString();
+            i.putExtra("descripcion",descCampo.getText());
             i.putExtra("fecha",fecha.getDayOfMonth()).toString();
+
+            i.putExtra(getString(R.string.pFecha_nacimientoYear), year);
+            i.putExtra(getString(R.string.pFecha_nacimientoMonth), month);
+            i.putExtra(getString(R.string.pFecha_nacimientoDay), day);
 
         startActivity(i);
     }
--- app/src/main/java/com/example/javier/abmcontactos/Actividad2.java
+++ app/src/main/java/com/example/javier/abmcontactos/Actividad2.java
@@ -5,41 +5,83 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Actividad2 extends AppCompatActivity {
 
     TextView tvNombre,tvTelefono,tvEmail,tvDesc,tvFecha;
+    private int intDia, intMes, intAño;
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad2);
 
-        tvNombre=(TextView)findViewById(R.id.tvNombreCampo);
-        tvTelefono=(TextView)findViewById(R.id.tvTelefonoCampo);
-        tvEmail=(TextView)findViewById(R.id.tvEmailCampo);
-        tvDesc=(TextView)findViewById(R.id.tvDescCampo);
-        tvFecha=(TextView) findViewById(R.id.tvFechaCampo);
-
         Intent intent=getIntent();
         Bundle par =intent.getExtras();
 
         if (par != null) {
 
+
+            tvNombre=(TextView)findViewById(R.id.tvNombreCampo);
+            tvTelefono=(TextView)findViewById(R.id.tvTelefonoCampo);
+            tvEmail=(TextView)findViewById(R.id.tvEmailCampo);
+            tvDesc=(TextView)findViewById(R.id.tvDescCampo);
+            tvFecha=(TextView) findViewById(R.id.tvFechaCampo);
+
+
             String datoNombre=par.getString("nombre");
             String datoTelefono=par.getString("telefono");
             String datoEmail=par.getString("eMail");
-            String datoDesc=par.getString("desc");
+            String datoDesc=par.getString("descripcion");
             String datoFecha=par.getString("fecha");
+
+            intAño = par.getInt(getString(R.string.pFecha_nacimientoYear));
+            intDia = par.getInt(getString(R.string.pFecha_nacimientoDay));
+            intMes = par.getInt(getString(R.string.pFecha_nacimientoMonth));
+
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.YEAR, intAño);
+            cal.set(Calendar.DAY_OF_MONTH, intDia);
+            cal.set(Calendar.MONTH, intMes);
+            String format = new SimpleDateFormat("E, MMM d, yyyy").format(cal.getTime());
 
             tvNombre.setText(datoNombre);
             tvTelefono.setText(datoTelefono);
             tvEmail.setText(datoEmail);
-            tvDesc.setText(datoDesc);
-            tvFecha.setText(datoFecha);
+
+            tvFecha.setText(format);
+
+            //Buscamos el boton
+            boton=(Button)findViewById(R.id.botonAtras);
+            boton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(Actividad2.this, Actividad1.class);
+                    /*intAño = ((DatePicker) findViewById(R.id.campoFecha)).getYear();
+                    intMes = ((DatePicker) findViewById(R.id.campoFecha)).getMonth();
+                    intDia = ((DatePicker) findViewById(R.id.campoFecha)).getDayOfMonth();*/
+
+                    intent.putExtra("nombre",tvNombre.getText().toString());
+                    intent.putExtra("telefono",tvTelefono.getText().toString());
+                    intent.putExtra("eMail",tvEmail.getText().toString()).toString();
+                    intent.putExtra("descripcion",tvDesc.getText());
+
+                    intent.putExtra(getString(R.string.pFecha_nacimientoYear), intAño);
+                    intent.putExtra(getString(R.string.pFecha_nacimientoMonth), intMes);
+                    intent.putExtra(getString(R.string.pFecha_nacimientoDay), intDia);
+
+                    startActivity(intent);
+                }
+            });
         }
     }
 }
